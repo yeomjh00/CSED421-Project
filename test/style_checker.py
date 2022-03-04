b5777802cfac7c4e20a389037ed16ed66e3c3b2f
dblab-ta <99519297+dblab-ta@users.noreply.github.com>
2022-03-04 21:39:39 +0900

    Add style check base score

--- test/style_checker.py
+++ test/style_checker.py
@@ -14,10 +14,11 @@ with open(output_file) as f:
             error_score = 20
             error_count = line[len(str_prefix):]
             error_score = error_score - 1 * int(error_count)
+            error_score = error_score + 5
 
             if error_score < 0:
                 error_score = 0
-            elif error_score >= 15:
+            elif error_score >= 20:
                 error_score = 20
 
 sys.exit(error_score)
